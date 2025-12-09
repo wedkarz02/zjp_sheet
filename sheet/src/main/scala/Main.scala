94bb54c1d1df8a30bdaecccc7d5f6dfb8c55bc15
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-12-09 19:03:04 +0100

    zad kol

--- sheet/src/main/scala/Main.scala
+++ sheet/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import scala.annotation.tailrec
+import scala.io.Source
 
 // LAB 1
 
@@ -260,7 +261,158 @@ def swap[A](seq: Seq[A]): Seq[A] = {
   println(swap(Seq(-3, 0, 1, 2, 5, 6)))
 }
 
-@main def main(): Unit = {
-  val x = 7
-  println(s"${x} + ${x} == ${x + x}")
+// KOL VER 1
+
+// def group[A, B, C](l: List[A])(f: A => B)(op: A => B)(op2: (A, C) => C): Set[(B, C)] = {
+//   @tailrec
+//   def helper(
+//       remaining: List[A],
+//       accEven: List[A],
+//       accOdd: List[A],
+//       evenResult: Option[C],
+//       oddResult: Option[C]
+//   ): (Option[C], Option[C]) = remaining match {
+//     case Nil => (evenResult, oddResult)
+//
+//     case head :: tail if f(head) == 0 =>
+//       val newEvenResult = evenResult match {
+//         case None => op(head)
+//         case Some(value) => op2(head, value)
+//       }
+//       helper(tail, head :: accEven, accOdd, Some(newEvenResult), oddResult)
+//
+//     case head :: tail =>
+//       val newOddResult = oddResult match {
+//         case None => op(head)
+//         case Some(value) => op2(head, value)
+//       }
+//       helper(tail, accEven, head :: accOdd, evenResult, Some(newOddResult))
+//   }
+//
+//   val (evenOpt, oddOpt) = helper(l, Nil, Nil, None, None)
+//
+//   val resultSet = (evenOpt, oddOpt) match {
+//     case (Some(evenSum), Some(oddSum)) => Set((0, evenSum), (1, oddSum))
+//     case (Some(evenSum), None) => Set((0, evenSum))
+//     case (None, Some(oddSum)) => Set((1, oddSum))
+//     case _ => Set.empty[(B, C)]
+//   }
+//
+//   resultSet
+// }
+
+def group[A, B, C](l: List[A])(f: A => B)(op: A => C)(op2: (A, C) => C): Set[(B, C)] = {
+  @tailrec
+  def helper(
+      remaining: List[A],
+      results: Map[B, C]
+  ): Map[B, C] = remaining match {
+    case Nil => results
+
+    case head :: tail =>
+      val key = f(head)
+      val newValue = results.get(key) match {
+        case None => op(head)
+        case Some(r) => op2(head, r)
+      }
+      helper(tail, results + (key -> newValue))
+  }
+
+  helper(l, Map.empty[B, C]).map { case (k, v) => (k, v) }.toSet
+}
+
+@main def kol1_main_zad1(): Unit = {
+  val l = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  val f = (x: Int) => x % 2
+  val op = (x: Int) => x + 0
+  val op2 = (x: Int, y: Int) => x + y
+
+  println(group(l)(f)(op)(op2))
+
+  // val result = group(List(1, 2, 3, 4, 5, 6, 7, 8, 9))(_ % 2)(_ + 0)(_ + _)
+  // println(s"${result}")
+}
+
+// object Zadanie2 {
+//   def main(args: Array[String]): Unit = {
+//     // Wczytanie danych z plików
+//     val statkiLines = Source.fromFile("statki.txt").getLines().toList
+//     val przybyciaLines = Source.fromFile("przybycia.txt").getLines().toList
+//
+//     // Utworzenie mapy identyfikator -> (nazwa, ladownosc) z pliku statki.txt
+//     val statkiMap = (for {
+//       line <- statkiLines
+//       parts = line.split(";").map(_.trim)
+//       if parts.length == 3
+//     } yield parts(0) -> (parts(1), parts(2).toInt)).toMap
+//
+//     // Przetworzenie danych z przybycia.txt i połączenie z danymi o statkach
+//     val przybyciaStatki = for {
+//       line <- przybyciaLines
+//       parts = line.split(";").map(_.trim)
+//       if parts.length == 3
+//       id = parts(1)
+//       if statkiMap.contains(id)
+//       (nazwaStatku, ladownosc) = statkiMap(id)
+//       nabrzeze = parts(2)
+//     } yield (nabrzeze, nazwaStatku, ladownosc)
+//
+//     // Grupowanie po nabrzeżach i znajdowanie maksymalnej ładowności dla każdego nabrzeża
+//     // bez użycia deprecated mapValues
+//     val maxLadownosci = przybyciaStatki
+//       .map { case (nabrzeze, _, ladownosc) => nabrzeze -> ladownosc }
+//       .groupBy(_._1)
+//       .map { case (nabrzeze, ladownosci) =>
+//         nabrzeze -> ladownosci.map(_._2).max
+//       }
+//
+//     // Utworzenie ostatecznej mapy: (nabrzeze, maxLadownosc) -> Set(nazwy statków)
+//     val wynik = (for {
+//       (nabrzeze, nazwaStatku, ladownosc) <- przybyciaStatki
+//       maxLadownosc = maxLadownosci(nabrzeze)
+//       if ladownosc == maxLadownosc
+//     } yield ((nabrzeze, maxLadownosc), nazwaStatku))
+//       .groupBy(_._1)  // Grupowanie po kluczu (nabrzeze, maxLadownosc)
+//       .map { case (klucz, statki) => klucz -> statki.map(_._2).toSet }
+//
+//     // Konwersja do HashMap dla prezentacji
+//     val wynikHashMap = HashMap(wynik.toSeq: _*)
+//
+//     // Prezentacja wyniku
+//     println("HashMap (" + wynikHashMap.mkString(", ") + ")")
+//   }
+// }
+
+@main def kol1_main_zad2(): Unit = {
+  val wynik = {
+    val statki = Source
+      .fromFile("statki.txt")
+      .getLines()
+      .map(_.split(";").map(_.trim))
+      .collect { case Array(id, nazwa, ladownosc) => id -> (nazwa, ladownosc.toInt) }
+      .toMap
+
+    val przybycia = Source
+      .fromFile("przybycia.txt")
+      .getLines()
+      .map(_.split(";").map(_.trim))
+      .collect {
+        case Array(_, id, nabrzeze) if statki.contains(id) =>
+          val (nazwaStatku, ladownosc) = statki(id)
+          (nabrzeze, nazwaStatku, ladownosc)
+      }
+      .toList
+
+    val maxLadownosci = przybycia
+      .map { case (nabrzeze, _, ladownosc) => nabrzeze -> ladownosc }
+      .groupBy(_._1)
+      .map { case (nabrzeze, ladownosci) => nabrzeze -> ladownosci.map(_._2).max }
+
+    przybycia
+      .filter { case (nabrzeze, _, ladownosc) => ladownosc == maxLadownosci(nabrzeze) }
+      .groupBy { case (nabrzeze, _, ladownosc) => (nabrzeze, ladownosc) }
+      .map { case (klucz, statki) => klucz -> statki.map(_._2).toSet }
+  }
+
+  println(wynik)
 }
